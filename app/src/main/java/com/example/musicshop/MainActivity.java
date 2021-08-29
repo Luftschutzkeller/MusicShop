778034a7b6382d5e23b9b261280f7416c0488143
Irina Keil <35578733+Luftschutzkeller@users.noreply.github.com>
2021-08-29 21:20:31 +0300

    решить вопросы с ошибками и цветами

--- app/src/main/java/com/example/musicshop/MainActivity.java
+++ app/src/main/java/com/example/musicshop/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.musicshop;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.Context;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getWindow().setStatusBarColor(ContextCompat.getColor(MainActivity.this, R.color.design_default_color_primary_variant));
     }
 }
