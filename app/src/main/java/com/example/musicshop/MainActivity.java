ac6edf7ccd055407c14087661feb71819fe5ebb2
Irina Keil <35578733+Luftschutzkeller@users.noreply.github.com>
2021-09-05 00:49:16 +0300

    public void addToCart

--- app/src/main/java/com/example/musicshop/MainActivity.java
+++ app/src/main/java/com/example/musicshop/MainActivity.java
@@ -6,17 +6,20 @@ import androidx.core.content.ContextCompat;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     int quantity =0;
+    EditText userNameEditText;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        userNameEditText = findViewById(R.id.nameEditText);
 
            }
 
@@ -24,5 +27,13 @@ public class MainActivity extends AppCompatActivity {
         quantity = quantity + 1;
         TextView quantityTextView = findViewById(R.id.quantityTextView);
         quantityTextView.setText("" + quantity);
+    }
+
+    public void addToCart(View view) {
+        Order order = new Order();
+        order.userName = userNameEditText.getText().toString();
+
+
+
     }
 }
