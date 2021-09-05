931c1cdae564677add3ae8475bb77ea344df3cf5
Irina Keil <35578733+Luftschutzkeller@users.noreply.github.com>
2021-09-05 23:08:37 +0300

    вернула весь код

--- app/src/main/java/com/example/musicshop/MainActivity.java
+++ app/src/main/java/com/example/musicshop/MainActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -16,16 +19,15 @@ import java.util.*;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
-    int quantity =0;
+    int quantity = 0;
     Spinner spinner;
-    ArrayList spinnerArraylist;
+    ArrayList spinnerArrayList;
     ArrayAdapter spinnerAdapter;
 
     HashMap goodsMap;
-
     String goodsName;
-
     double price;
+    EditText userNameEditText;
 
 
     @Override
@@ -33,33 +35,42 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        userNameEditText = findViewById(R.id.nameEditText);
+
+        createSpinner();
+
+        createMap();
+
+    }
+
+    void createSpinner() {
         spinner = findViewById(R.id.spinner);
         spinner.setOnItemSelectedListener(this);
-        spinnerArraylist = new ArrayList();
+        spinnerArrayList = new ArrayList();
 
-        spinnerArraylist.add("guitar");
-        spinnerArraylist.add("drums");
-        spinnerArraylist.add("keyboard");
-        spinnerArraylist.add("notes");
+        spinnerArrayList.add("guitar");
+        spinnerArrayList.add("drums");
+        spinnerArrayList.add("keyboard");
 
-        spinnerAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, spinnerArraylist);
+        spinnerAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, spinnerArrayList);
         spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(spinnerAdapter);
+    }
 
+    void createMap() {
         goodsMap = new HashMap();
         goodsMap.put("guitar", 500.0);
         goodsMap.put("drums", 1500.0);
         goodsMap.put("keyboard", 1000.0);
-        goodsMap.put("notes", 700.0);
+    }
 
-
-
-           }
 
     public void increaseQuantity(View view) {
         quantity = quantity + 1;
         TextView quantityTextView = findViewById(R.id.quantityTextView);
         quantityTextView.setText("" + quantity);
+        TextView priceTextView = findViewById(R.id.priceTextView);
+        priceTextView.setText("" + quantity * price);
     }
 
     public void decreaseQuantity(View view) {
@@ -69,21 +80,62 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         }
         TextView quantityTextView = findViewById(R.id.quantityTextView);
         quantityTextView.setText("" + quantity);
-
+        TextView priceTextView = findViewById(R.id.priceTextView);
+        priceTextView.setText("" + quantity * price);
     }
 
     @Override
-    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         goodsName = spinner.getSelectedItem().toString();
         price = (double)goodsMap.get(goodsName);
         TextView priceTextView = findViewById(R.id.priceTextView);
         priceTextView.setText("" + quantity * price);
 
+        ImageView goodsImageView = findViewById(R.id.goodsImageView);
+
+        switch (goodsName) {
+            case "guitar":
+                goodsImageView.setImageResource(R.drawable.guitar);
+                break;
+            case "drums":
+                goodsImageView.setImageResource(R.drawable.drums);
+                break;
+            case "keyboard":
+                goodsImageView.setImageResource(R.drawable.keyboard);
+                break;
+            default:
+                goodsImageView.setImageResource(R.drawable.guitar);
+                break;
+        }
+
     }
 
     @Override
-    public void onNothingSelected(AdapterView<?> adapterView) {
+    public void onNothingSelected(AdapterView<?> parent) {
 
+    }
 
+    public void addToCart(View view) {
+
+        Order order = new Order();
+
+        order.userName = userNameEditText.getText().toString();
+
+        order.goodsName = goodsName;
+
+        order.quantity = quantity;
+
+        order.price = price;
+
+        order.orderPrice = quantity * price;
+
+        Intent orderIntent = new Intent(MainActivity.this, OrderActivity.class);
+        orderIntent.putExtra("userNameForIntent", order.userName);
+        orderIntent.putExtra("goodsName", order.goodsName);
+        orderIntent.putExtra("quantity", order.quantity);
+        orderIntent.putExtra("price", order.price);
+        orderIntent.putExtra("orderPrice", order.orderPrice);
+
+        startActivity(orderIntent);
     }
 }
